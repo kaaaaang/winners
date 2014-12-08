ec787921779ed4172a96211876fc0e781c5d6991
Kang <akang@c82a14439e87.ant.amazon.com>
2014-12-08 09:35:14 -0800

    Adding origin filter

---
+++ src/main/java/com/zappos/winner/filter/AllowOriginFilter.java
@@ -0,0 +1,25 @@
+package com.zappos.winner.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: akang
+ * Date: 12/8/14
+ * Time: 9:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllowOriginFilter implements Filter {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+}
